32c5d475cc5f4737ca916bd86bd5b6a70da38fbb
Lorien Henry-Wilkins <lorien.henrywilkins@gmail.com>
2015-05-11 11:16:16 -0700

    a new Gruntfile for mobile, so the mobile tests can be run separately on sauce, with slower timeouts

---
+++ Gruntfile_mobile.js
@@ -0,0 +1,114 @@
+module.exports = function(grunt) {
+  // Do grunt-related things in here
+
+  grunt.loadNpmTasks('grunt-saucelabs');
+
+  // Project configuration.
+  grunt.initConfig({
+    'saucelabs-custom': {
+      all: {
+        options: {
+          "max-duration": 2400,
+          "throttled": 3,
+          "tunneled": true,
+          urls: [
+            'http://localhost:8080/saucerun?maxChecks=70&testTimeout=2000'
+          ],
+          testname: 'DREEM smoke test mobile',
+
+          browsers: [
+            {
+              browserName: 'android',
+              version: '5.1',
+              platform: 'Linux',
+              deviceName: 'Android Emulator',
+              'device-orientation': 'portrait'
+            },
+            {
+              browserName: 'iphone',
+              version: '8.2',
+              platform: 'OS X 10.10',
+              deviceName: 'iPhone Simulator',
+              'device-orientation': 'portrait'
+            }
+
+//            {
+//              browserName: 'ipad',
+//              version: '8.1',
+//              platform: 'OS X 10.9',
+//              deviceName: 'iPad Simulator',
+//              'device-orientation': 'portrait'
+//            }//,
+
+//            {
+//              browserName: 'android',
+//              version: '4.3',
+//              platform: 'Linux',
+//              deviceName: 'Android Emulator',
+//              'device-orientation': 'portrait'
+//            },
+//            {
+//              browserName: 'android',
+//              version: '4.2',
+//              platform: 'Linux',
+//              deviceName: 'Android Emulator',
+//              'device-orientation': 'portrait'
+//            },
+//            {
+//              browserName: 'android',
+//              version: '4.1',
+//              platform: 'Linux',
+//              deviceName: 'Android Emulator',
+//              'device-orientation': 'portrait'
+//            },
+//            {
+//              browserName: 'android',
+//              version: '4.0',
+//              platform: 'Linux',
+//              deviceName: 'Android Emulator',
+//              'device-orientation': 'portrait'
+//            },
+//            {
+//              browserName: 'android',
+//              version: '4.4',
+//              platform: 'Linux',
+//              deviceName: 'Google Nexus 7 HD Emulator',
+//              'device-orientation': 'portrait'
+//            },
+//            {
+//              browserName: 'android',
+//              version: '4.4',
+//              platform: 'Linux',
+//              deviceName: 'Google Nexus 7C Emulator',
+//              'device-orientation': 'portrait'
+//            },
+//            {
+//              browserName: 'android',
+//              version: '4.0',
+//              platform: 'Linux',
+//              deviceName: 'HTC Evo 3D Emulator',
+//              'device-orientation': 'portrait'
+//            },
+//            {
+//              browserName: 'android',
+//              version: '4.1',
+//              platform: 'Linux',
+//              deviceName: 'HTC One X Emulator',
+//              'device-orientation': 'portrait'
+//            },
+//            {
+//              browserName: 'android',
+//              version: '4.4',
+//              platform: 'Linux',
+//              deviceName: 'LG Nexus 4 Emulator',
+//              'device-orientation': 'portrait'
+//            }
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['saucelabs-custom']);
+
+};
